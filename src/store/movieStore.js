0bb3a63cc017481b778bd793ec5aab4655f2fa47
Ratu <ratukf@gmail.com>
2026-03-05 18:31:05 +0700

    feat(store): add more state, setter, and custom setter

--- src/store/movieStore.js
+++ src/store/movieStore.js
@@ -1,9 +1,35 @@
 import { create } from "zustand";
 
 const useMovieStore = create((set) => ({
+  // State
   movies: [],
+  page: 0,
+  totalPage: 0,
+  total: 0,
+  isLoading: false,
+  error: null,
 
+  // State setter
   setMovies: (movies) => set({ movies }),
+  setPage: (page) => set({ page }),
+  setTotal: (total) => set({ total }),
+  setTotalPage: (totalPage) => set({ totalPage }),
+  setIsLoading: (isLoading) => set({ isLoading }),
+  setError: (error) => set({ error }),
+
+  // Custom state setter (Create, Update, Delete)
+  addMovie: (movie) =>
+    set((state) => ({
+      movies: [movie, ...state.movies],
+    })),
+  updateMovie: (id, updated) =>
+    set((state) => ({
+      movies: state.movies.map((m) => (m.id === id ? { ...m, ...updated } : m)),
+    })),
+  deleteMovie: (id) =>
+    set((state) => ({
+      movies: state.movies.filter((m) => m.id !== id),
+    })),
 }));
 
 export { useMovieStore };
