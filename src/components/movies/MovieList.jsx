a7fcfbff94d7aa120e35acedf750204696477991
Ratu <ratukf@gmail.com>
2026-03-05 22:00:46 +0700

    feat(component): develop movie list

--- src/components/movies/MovieList.jsx
+++ src/components/movies/MovieList.jsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { MovieCard } from "@/components/movies/MovieCard";
+import { useMovieStore } from "@/store/movieStore";
+
 const MovieList = () => {
-  return <div>MovieList</div>;
+  const { movies } = useMovieStore();
+  return (
+    <div>
+      <h4>MovieList</h4>
+      {movies.map((item) => (
+        <MovieCard
+          id={item.id}
+          key={item.id}
+          poster_path={item.poster_path}
+          title={item.title}
+          release_date={item.release_date}
+          vote_average={item.vote_average}
+          genre_ids={item.genre_ids}
+        />
+      ))}
+    </div>
+  );
 };
 
 export { MovieList };
