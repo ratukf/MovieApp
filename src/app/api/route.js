015f3dab24cab5d892b22964f82b8596eb196b91
Ratu <ratukf@gmail.com>
2026-03-05 23:40:16 +0700

    chore(route): add page params

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,12 +1,15 @@
 import { NextResponse } from "next/server";
 
-const GET = async () => {
+const GET = async (request) => {
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get("page") || 1;
+
   const [moviesRes, genresRes] = await Promise.all([
     fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/movie/popular?api_key=${process.env.API_KEY}`,
+      `${process.env.API_URL}/movie/popular?api_key=${process.env.API_KEY}&page=${page}`,
     ),
     fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/genre/movie/list?api_key=${process.env.API_KEY}`,
+      `${process.env.API_URL}/genre/movie/list?api_key=${process.env.API_KEY}`,
     ),
   ]);
 
