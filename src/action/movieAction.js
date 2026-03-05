27e2764c38d348f9634413c6dd2b8602ab50d260
Ratu <ratukf@gmail.com>
2026-03-05 18:31:39 +0700

    chore(action): add setter to movie action

--- src/action/movieAction.js
+++ src/action/movieAction.js
@@ -1,9 +1,35 @@
+import { useMovieStore } from "@/store/movieStore";
+
 const { fetchMoviesService } = require("@/service/movieService");
 
 const fetchMovieAction = async () => {
+  const { setMovies, setPage, setTotal, setTotalPage, setIsLoading, setError } =
+    useMovieStore.getState();
+
+  setIsLoading(true);
+  setError(null);
+
   try {
-    await fetchMoviesService();
-  } catch (err) {}
+    const res = await fetchMoviesService();
+
+    setMovies(res.results);
+    setIsLoading(false);
+  } catch (err) {
+    setMovies({});
+    setIsLoading(false);
+    // Error state handler for each different response status
+    if (!navigator.onLine) {
+      setError("No internet connection.");
+    } else if (err.response?.status === 401) {
+      setError("API key is not valid.");
+    } else if (err.response?.status === 404) {
+      setError("Data not found.");
+    } else if (err.response?.status >= 500) {
+      setError("Server is error, try again later");
+    } else {
+      setError("Error, try again");
+    }
+  }
 };
 
 export { fetchMovieAction };
