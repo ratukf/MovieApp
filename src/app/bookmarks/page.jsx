ce2aa9b99a4387304aa76a8f288193c819f9af06
Ratu <ratukf@gmail.com>
2026-03-05 23:16:10 +0700

    feat: develop bookmark page

--- src/app/bookmarks/page.jsx
+++ src/app/bookmarks/page.jsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { MovieCard } from "@/components/movies/MovieCard";
+import { useMovieStore } from "@/store/movieStore";
+
 const BookmarksPage = () => {
-  return <div>Bookmarks</div>;
+  const { movies, bookmarks } = useMovieStore();
+  const bookmark = movies.filter((item) => bookmarks.includes(item.id));
+
+  return (
+    <div>
+      {bookmark.map((item) => (
+        <MovieCard
+          id={item.id}
+          key={item.id}
+          poster_path={item.poster_path}
+          title={item.title}
+          release_date={item.release_date}
+          vote_average={item.vote_average}
+          genre_ids={item.genre_ids}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BookmarksPage;
