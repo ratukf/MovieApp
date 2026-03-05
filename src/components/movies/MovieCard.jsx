a469c0589f1b0d6089dd554005d9d9df74e1c1cc
Ratu <ratukf@gmail.com>
2026-03-05 22:00:29 +0700

    feat(component): develop movie card

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -1,5 +1,36 @@
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+const MovieCard = ({
+  id,
+  poster_path,
+  title,
+  release_date,
+  vote_average,
+  genre_ids,
+}) => {
+  const year = release_date?.split("-")[0];
+
+  return (
+    <div>
+      <Link href={`/movies/${id}`}>
+        <Image
+          alt={title}
+          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          width={300}
+          height={450}
+        />
+        <p>{title}</p>
+        <p>{year}</p>
+        <p>{vote_average}</p>
+        <div>
+          {genre_ids.map((id) => (
+            <span key={id}>{id}</span>
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export { MovieCard };
