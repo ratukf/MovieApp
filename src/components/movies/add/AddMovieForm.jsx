6d2300f4d25550416c8669d908fb0723ab71ba55
Ratu <ratukf@gmail.com>
2026-03-05 22:56:59 +0700

    feat: develop add movie form

--- src/components/movies/add/AddMovieForm.jsx
+++ src/components/movies/add/AddMovieForm.jsx
@@ -1,5 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useMovieStore } from "@/store/movieStore";
+import { MovieForm } from "@/components/movies/MovieForm";
+
 const AddMovieForm = () => {
-  return <div>AddMovieForm</div>;
+  const router = useRouter();
+  const { addMovie } = useMovieStore();
+
+  const handleSubmit = (values) => {
+    addMovie({
+      ...values,
+      id: Date.now(),
+      vote_average: Number(values.vote_average),
+    });
+    router.push("/movies");
+  };
+
+  return (
+    <div>
+      <button onClick={() => router.back()}>Back</button>
+      <MovieForm onSubmit={handleSubmit} />
+    </div>
+  );
 };
 
 export { AddMovieForm };
