5f1b1b942951cc5553869f2fc24e657ec1471b78
Ratu <ratukf@gmail.com>
2026-03-05 22:35:41 +0700

    chore(api): add route to fetch genre

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,12 +1,19 @@
 import { NextResponse } from "next/server";
 
 const GET = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/popular?api_key=${process.env.API_KEY}`,
-  );
-  const data = await res.json();
+  const [moviesRes, genresRes] = await Promise.all([
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/movie/popular?api_key=${process.env.API_KEY}`,
+    ),
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/genre/movie/list?api_key=${process.env.API_KEY}`,
+    ),
+  ]);
 
-  return NextResponse.json(data);
+  const movies = await moviesRes.json();
+  const genres = await genresRes.json();
+
+  return NextResponse.json({ movies, genres });
 };
 
 export { GET };
